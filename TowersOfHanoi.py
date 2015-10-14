ef2d32677a5931e389e1e40a359b050648ad73a3
DnLKnR <konia013@umn.edu>
2015-10-13 20:47:26 -0500

    Issue #2 is fixed.  An explored set has been constructed and is now used.  May need to look into faster explored set options, but for now it is sufficient

--- TowersOfHanoi.py
+++ TowersOfHanoi.py
@@ -6,13 +6,19 @@ import queue,copy,sys
 """This is where the problem is defined. Initial state, goal state and other information that can be got from the problem"""
 class Towers_Of_Hanoi(object):
     
-    def __init__(self, length=3, height=3):
+    def __init__(self, length=3, height=3, is_swapped=False):
         """This is the constructor for the Problem class. It specifies the initial state, and possibly a goal state, if there is a unique goal.  You can add other arguments if the need arises"""
         self.length = length
         self.height = height
-        self.__construct__()
+        self.explored = []
+        self.__construct__(is_swapped)
     
-    def __construct__(self):
+    def __construct__(self, is_swapped):
+        """This is the function that constructs the initial state and 
+        the goal state for the towers of hanoi.  Note: One of the 
+        Bidirectional's Towers of Hanoi setup needs the goal state and 
+        the initial state swapped, thus there is an extra parameter to
+        perform this action."""
         initial = []
         goal = []
         for i in range(self.length):
@@ -21,8 +27,13 @@ class Towers_Of_Hanoi(object):
         for i in range(self.height,0,-1):
             initial[0].push(i)
             goal[-1].push(i)
-        self.initial = initial
-        self.goal = goal
+        
+        if is_swapped:
+            self.initial = goal
+            self.goal = initial
+        else:
+            self.initial = initial
+            self.goal = goal
     
     def actions(self, state):
         """Return the actions that can be executed in the given
@@ -30,6 +41,8 @@ class Towers_Of_Hanoi(object):
         many actions, consider yielding them one at a time in an
         iterator, rather than building them all at once."""
         action = []
+        if self.is_explored(state):
+            return action
         for i in range(self.length):
             if state[i].get_size() > 0:
                 for j in range(self.length):
@@ -39,32 +52,45 @@ class Towers_Of_Hanoi(object):
                         action.append([i,j])
                     elif state[i].peek() < state[j].peek():
                         action.append([i,j])
-        #print(action)
+        self.explored.append(state)
         return action
 	
-    def result(self, state, action):
-        """Return the state that results from executing the given
-        action in the given state. The action must be one of
-        self.actions(state)."""
+    def copy(self, state):
         new_state = []
         for tower in state:
             new_tower = tower.copy()
             new_state.append(new_tower)
+        return new_state
+    
+    def result(self, state, action):
+        """Return the state that results from executing the given
+        action in the given state. The action must be one of
+        self.actions(state)."""
+        new_state = self.copy(state)
         i,j = action
         value = new_state[i].pop()
         new_state[j].push(value)
         return new_state
-
+    
+    def is_explored(self, state):
+        for explored_state in self.explored[::-1]:
+            if self.compare(explored_state,state):
+                return True
+        return False
+    
+    def compare(self, state, other):
+        for i in range(self.length):
+            if not state[i].compare(other[i]):
+                return False
+        return True
+    
     def goal_test(self, state):
         """Return True if the state is a goal. The default method compares the
         state to self.goal, as specified in the constructor. Override this
         method if checking against a single self.goal is not enough.
         This must be written by students"""
-        if state[-1].get_size() != self.height:
+        if not self.compare(state, self.goal):
             return False
-        for i in range(self.length):
-            if not self.goal[i].compare(state[i]):
-                return False
         return True
 		
 class Node(object):
@@ -94,10 +120,7 @@ class Node(object):
 		return Node(next, self, action)
 
 
-def bidirectional_search(problem, solution):
-    pass
-    
-def breadth_first_search(problem):
+def bidirectional_search(problem,solution):
     # Start from first node of the problem Tree
     node = Node(problem.initial)
     # Check if current node meets Goal_Test criteria
@@ -108,7 +131,7 @@ def breadth_first_search(problem):
     frontier.put_nowait(node)
 
     ## QUESTION 3 NODE QUANTIFIER ##
-    ## node_counter = 1
+    node_counter = 1
     
     print_towers(node.state)
     # Loop until all nodes are explored(frontier queue is empty) or Goal_Test criteria are met
@@ -123,14 +146,56 @@ def breadth_first_search(problem):
         for child in node.expand(problem):
 
             ## QUESTION 3 NODE QUANTIFIER ##
-            ## node_counter += 1
+            node_counter += 1
 
             # If child node meets Goal_Test criteria
             if problem.goal_test(child.state):
                 print("Solution found: ")
                 print_towers(child.state)
                 ## QUESTION 3 NODE QUANTIFIER ##
-                ## print("\n\nNodes Created: " + str(node_counter))
+                print("\n\nNodes Created: " + str(node_counter))
+
+                return child
+            # Add every new child to the frontier
+            #print("Mediary: ")
+            #print_towers(child.state)
+            frontier.put_nowait(child)
+    return None
+    
+def breadth_first_search(problem):
+    # Start from first node of the problem Tree
+    node = Node(problem.initial)
+    # Check if current node meets Goal_Test criteria
+    if problem.goal_test(node.state):
+        return node
+    # Create a Queue to store all nodes of a particular level. Import QueueClass()
+    frontier=queue.Queue()
+    frontier.put_nowait(node)
+
+    ## QUESTION 3 NODE QUANTIFIER ##
+    node_counter = 1
+    
+    print_towers(node.state)
+    # Loop until all nodes are explored(frontier queue is empty) or Goal_Test criteria are met
+    while frontier:
+        # if queue is empty, there is no solution
+        if frontier.qsize() == 0:
+            return None
+        # Remove from frontier, for analysis
+        node = frontier.get_nowait()
+        # Loop over all children of the current node
+        # Note: We consider the fact that a node can have multiple child nodes here
+        for child in node.expand(problem):
+
+            ## QUESTION 3 NODE QUANTIFIER ##
+            node_counter += 1
+
+            # If child node meets Goal_Test criteria
+            if problem.goal_test(child.state):
+                print("Solution found: ")
+                print_towers(child.state)
+                ## QUESTION 3 NODE QUANTIFIER ##
+                print("\n\nNodes Created: " + str(node_counter))
 
                 return child
             # Add every new child to the frontier
@@ -148,7 +213,7 @@ def print_towers(towers):
 
 
 
-breadth_first_search(Towers_Of_Hanoi(length=3,height=4))
+breadth_first_search(Towers_Of_Hanoi(length=3,height=7))
 
 
 
